754a5bce5c59d933077834bc886a2d8d4b1e2fe0
Ricardo Brito <ricardo.b@msn.com>
2018-07-25 15:09:53 -0300

    Add project files.

---
+++ RdRand.ConsoleTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SilverCard;
+
+namespace SilverCard.RdRandConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "RdRand.ConsoleTest";                        
+
+            if (RdRand.IsRdRandSupported())
+            {
+                try
+                {
+                    Console.WriteLine($"Testing {nameof(RdRand.Rand16)}.");
+                    for (int i = 0; i < 10; i++) Console.WriteLine(RdRand.Rand16());
+
+                    Console.WriteLine($"Testing {nameof(RdRand.Rand32)}.");
+                    for (int i = 0; i < 10; i++) Console.WriteLine(RdRand.Rand32());
+
+                    Console.WriteLine($"Testing {nameof(RdRand.Rand64)}.");
+                    for (int i = 0; i < 10; i++) Console.WriteLine(RdRand.Rand64());
+
+                    Console.WriteLine("Test complete.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else
+            {
+                Console.WriteLine("RdRand is not supported in this machine.");
+            }
+     
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RdRand/RandomGeneratorException.h
@@ -0,0 +1,13 @@
+using namespace System;
+
+#pragma once
+namespace SilverCard {
+	ref class RandomGeneratorException : Exception
+	{
+	public:
+		RandomGeneratorException() : Exception("rdrand did not generate a number.") {}
+		RandomGeneratorException(String ^ message) : Exception(message) {}
+		RandomGeneratorException(Exception ^ ex) : Exception("rdrand did not generate a number. See inner exception for details.", ex) {}
+	};
+}
+
